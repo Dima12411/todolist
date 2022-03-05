15c7fa3ff9ee9964dbcd17f44999dd44b4622813
Dima12411 <fixer40delta@gmail.com>
2022-03-05 17:25:03 +0300

    added a loader for more pleasant UI

---
+++ src/app/App.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect} from 'react'
+import './App.css'
+import { TodolistsList } from '../features/TodolistsList/TodolistsList'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { Menu } from '@mui/icons-material';
+import LinearProgress from '@mui/material/LinearProgress';
+import {useAppSelector} from "./store";
+import {RequestStatusType} from "./app-reducer";
+
+
+function App() {
+    const status = useAppSelector<RequestStatusType>(state => state.app.status)
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            {status === 'loading' && <LinearProgress color="secondary"/>}
+            <Container fixed>
+                <TodolistsList/>
+            </Container>
+        </div>
+    )
+}
+
+export default App
